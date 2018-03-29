759351eecd5f52d625514575f5138d139198c8a1
Alvaro Fernandez <fernandez-alvaro@hotmail.com>
2018-03-30 01:22:16 +0200

    nobody is gonna read this anyway

--- index.php
+++ index.php
@@ -9,13 +9,10 @@
 
 
 </head>
+<a name="intro" class="hidden"></a>
 <body>
-    <!-- Intro -->
-    <div class="testdiv">
-        test
-    </div>
-    <!-- Navbar -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <!-- Navbar fixed -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" id="navbar-fixed-hidden">
     <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Á</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,43 +21,84 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link localLink" href="#anch3">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link localLink" href="#" id="introLink">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link localLink" href="#anch4">Projects</a>
+                <a class="nav-link localLink" href="#about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link localLink" href="#anch5">Contact</a>
+                <a class="nav-link localLink" href="#current_proj">Projects</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link localLink" href="#experience">Experience</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link localLink" href="#contact">Contact</a>
             </li>
         </ul>
     </div>
     </div>
     </nav>
 
-<div class="main">
+    <!-- Intro section -->
+<div class="intro main">
+    <div class="container-fluid">
+
+        <!-- Intro -->
+        <div class="testdiv">
+            <h1>
+                > Á $ ~&nbsp;<a href="" class="typewrite" data-period="2000" data-type='[ "Welcome to my website.", "Enjoy!" ]'>
+                    <span class="wrap"></span>
+                </a>
+            </h1>
+        </div>
+    </div>
+</div>
+
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar-static">
+        <a name="about" class="">&nbsp;</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Á</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link localLink" href="#" id="introLink">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link localLink" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link localLink" href="#current_proj">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link localLink" href="#experience">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link localLink" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+<!-- Main section -->
+<div class="main sections">
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-1 col-md-1"></div>
             <div class="col-sm-10 col-md-10">
-                <!-- Intro -->
-                <a name="anch1" class="anchor">&nbsp;</a>
-                <h1>Intro</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </p>
-            </div>
-            <div class="col-sm-1 col-md-1"></div>
-        </div>
-        <div class="row">
-            <div class="col-sm-1 col-md-1"></div>
-            <div class="col-sm-10 col-md-10">
                 <!-- About -->
-                <a name="about" class="anchor">&nbsp;</a>
-                <h1>About</h1>
+                <a name="space" class="anchor">&nbsp;</a>
+                <section id="1"><h1>About</h1></section>
                 <p>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
@@ -75,7 +113,7 @@
             <div class="col-sm-10 col-md-10">
                 <!-- Current Projects -->
                 <a name="current_proj" class="anchor">&nbsp;</a>
-                <h1>Current Projects</h1>
+                <section id="2"><h1>Current Projects</h1></section>
                 <p>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
@@ -120,7 +158,7 @@
             <div class="col-sm-10 col-md-10">
                 <!-- Experience -->
                 <a name="experience" class="anchor">&nbsp;</a>
-                <h1>Experience</h1>
+                <section id="3"><h1>Experience</h1></section>
                 <p>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
@@ -135,7 +173,7 @@
             <div class="col-sm-10 col-md-10">
                 <!-- Contact -->
                 <a name="contact" class="anchor">&nbsp;</a>
-                <h1>Contact</h1>
+                <section id="4"><h1>Contact</h1></section>
                 <p>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
@@ -161,7 +199,7 @@
             </li>
             <li class="list-inline-item">
                 <a class="btn-floating btn-sm mx-1" href="https://www.linkedin.com/in/afdz1">
-                    <img class="social" src="res/img/linkedin.png">
+                    <img class="social" src="res/img/linkedin_black.png">
                 </a>
             </li>
         </ul>
@@ -179,6 +217,7 @@
 <!--/.Footer-->
 
 <script type="text/javascript">
+    // Smooth scrolling when clicking on links
     $(document).ready(function(){
         $('.localLink').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -194,6 +233,89 @@
                 }
             }
         });
+    });
+
+    // Fancy navbar shit
+    $(document).ready(function(){
+        $(window).on('scroll',function() {
+            var scrolltop = $(this).scrollTop();
+
+            var $nav = $("#navbar-static");
+            if(scrolltop >= $nav.offset().top + $nav.height()) {
+                $('#navbar-fixed-hidden').fadeIn(250);
+            }
+            else {
+                $('#navbar-fixed-hidden').fadeOut(250);
+            }
+        });
+    });
+
+    // scroll to top
+    jQuery('#introLink').click(function(e){
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For chrome, Firefox, IE and opera
+        return false;
+    });
+
+
+    //fancy typewriter shit
+    $(document).ready(function(){
+        var TxtType = function(el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
+
+        TxtType.prototype.tick = function() {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+            var that = this;
+            var delta = 200 - Math.random() * 100;
+
+            if (this.isDeleting) { delta /= 2; }
+
+            if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
+            }
+
+            setTimeout(function() {
+                that.tick();
+            }, delta);
+        };
+
+        window.onload = function() {
+            var elements = document.getElementsByClassName('typewrite');
+            for (var i=0; i<elements.length; i++) {
+                var toRotate = elements[i].getAttribute('data-type');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                    new TxtType(elements[i], JSON.parse(toRotate), period);
+                }
+            }
+            // INJECT CSS
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+            document.body.appendChild(css);
+        };
     });
 </script>
 
